953dee3ff0d9c194933f189853a6df0d5760b674
lezu311 <lezuwashere@gmail.com>
2026-09-03 20:38:33 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const WebSocket = require("ws");
 
 let rgb = {
-    r : 0,
-    g : 0,
-    b : 0
-}
+    r: 0,
+    g: 0,
+    b: 0
+};
 
 const PORT = process.env.PORT || 8080;
 
@@ -12,22 +12,20 @@ const server = new WebSocket.Server({
     port: PORT
 });
 
-function broadcast(data) {
-    const message = JSON.stringify(data)
-
-    server.clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(message)
-        }
-    })
-}
+server.on("listening", () => {
+    console.log("WEBSOCKET SERVER LISTENING ON PORT", PORT);
+});
 
 server.on("connection", (socket) => {
-    console.log("someone connected!");
+    console.log("================================");
+    console.log("CLIENT CONNECTED!");
+    console.log("================================");
 
     socket.send(JSON.stringify(rgb));
 
     socket.on("message", (message) => {
+        console.log("RECEIVED:", message.toString());
+
         if (message.toString() === "changecolor") {
             rgb.r = Math.random();
             rgb.g = Math.random();
@@ -35,9 +33,27 @@ server.on("connection", (socket) => {
 
             broadcast(rgb);
 
-            console.log("replied to client");
+            console.log("REPLIED TO CLIENT");
         }
     });
+
+    socket.on("close", () => {
+        console.log("CLIENT DISCONNECTED");
+    });
+
+    socket.on("error", (error) => {
+        console.log("SOCKET ERROR:", error);
+    });
 });
+
+function broadcast(data) {
+    const message = JSON.stringify(data);
+
+    server.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(message);
+        }
+    });
+}
 
 console.log("STARTING WEBSOCKET SERVER");
